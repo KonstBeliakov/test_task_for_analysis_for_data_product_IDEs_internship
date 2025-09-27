d6adfeb6a75123c87494df07ee020fcaf0e6eaa7
KonstBeliakov <konstbeliakov@gmail.com>
2025-09-27 14:19:49 +0300

    added histogram

--- analysis_toolwindow_usage.py
+++ analysis_toolwindow_usage.py
@@ -1,5 +1,9 @@
 import csv
 from dataclasses import dataclass
+import matplotlib
+
+matplotlib.use("TkAgg")
+
 import matplotlib.pyplot as plt
 
 
@@ -20,16 +24,13 @@ with (open("toolwindow_data.csv", "r", encoding="utf-8", newline="") as f):
     auto_durations = []
     manual_durations = []
 
-    manual_time = 0
-    manual_events = 0
-
     for row in reader:
         ts, event, open_type, user_id = row
         new_status = Status(int(ts), event, open_type, user_id)
 
         if user_id in users:
             old_status = users[user_id]
-            if old_status.event == 'opened' and new_status.event == 'closed'\
+            if old_status.event == 'opened' and new_status.event == 'closed' \
                     and old_status.timestamp < new_status.timestamp:
 
                 time = new_status.timestamp - old_status.timestamp
@@ -46,11 +47,15 @@ with (open("toolwindow_data.csv", "r", encoding="utf-8", newline="") as f):
 avg_auto_time = sum(auto_durations) / len(auto_durations) if auto_durations else 0
 avg_manual_time = sum(manual_durations) / len(manual_durations) if manual_durations else 0
 
-print(f'\n{avg_auto_time=}\n{avg_manual_time=}')
 
-# plt.hist(manual_durations, bins=50, alpha=0.5, label="Manual")
-# plt.hist(auto_durations, bins=50, alpha=0.5, label="Auto")
-# plt.xscale('log')
-# plt.legend()
-# plt.show()
+print(f'Auto openings: {len(auto_durations)}\tManual openings: {len(manual_durations)}')
+print(f'{avg_auto_time=}\n{avg_manual_time=}')
 
+plt.hist(manual_durations, bins=20, density=True, alpha=0.5, label='Manual')
+plt.hist(auto_durations, bins=20, density=True, alpha=0.5, label='Auto')
+
+plt.xlabel("Time")
+plt.ylabel("Amount")
+plt.title("Comparison of automatic and manual window opening times")
+plt.legend()
+plt.show()
