ba816a7f09a1e61179fd7a50aa402885acba7d6d
KonstBeliakov <konstbeliakov@gmail.com>
2025-09-27 22:54:01 +0300

    refactoring

--- analysis_toolwindow_usage.py
+++ analysis_toolwindow_usage.py
@@ -1,5 +1,7 @@
 import csv
 from dataclasses import dataclass
+from datetime import datetime, timedelta
+
 import matplotlib
 
 matplotlib.use("TkAgg")
@@ -37,37 +39,12 @@ with (open("toolwindow_data.csv", "r", encoding="utf-8", newline="") as f):
 
                 time = new_status.timestamp - old_status.timestamp
 
-                #print(old_status, new_status, time)
-
                 if old_status.open_type == 'auto':
                     auto_durations.append(time)
                 elif old_status.open_type == 'manual':
                     manual_durations.append(time)
 
         users[user_id] = new_status
-
-avg_auto_time = sum(auto_durations) / len(auto_durations) if auto_durations else 0
-avg_manual_time = sum(manual_durations) / len(manual_durations) if manual_durations else 0
-
-print(f'Auto openings: {len(auto_durations)}')
-print(f'Manual openings: {len(manual_durations)}\n')
-
-print(f'Median time for auto openings: {np.median(auto_durations)}')
-print(f'Median time for manual openings: {np.median(manual_durations)}\n')
-
-print(f'Average time for auto openings: {avg_auto_time}')
-print(f'Average time for manual openings: {avg_manual_time}\n')
-
-stat, p_value = mannwhitneyu(manual_durations, auto_durations, alternative='two-sided')
-
-print(f"\nMann-Whitney U statistic: {stat}")
-print(f"p-value: {p_value}")
-
-print(p_value)
-if p_value < 0.05:
-    print("The differences are statistically significant (p < 0.05)")
-else:
-    print("There are no statistically significant differences")
 
 
 def permutation_test_median(a, b, n=5000):
@@ -84,14 +61,6 @@ def permutation_test_median(a, b, n=5000):
     return (count + 1) / (n + 1)
 
 
-p_value = permutation_test_median(np.array(auto_durations), np.array(manual_durations))
-print("\nPermutation test p-value:", p_value)
-if p_value < 0.05:
-    print("The differences are statistically significant (p < 0.05)")
-else:
-    print("There are no statistically significant differences")
-
-
 def cliffs_delta(a, b):
     gt = 0
     lt = 0
@@ -101,42 +70,80 @@ def cliffs_delta(a, b):
     n = len(a) * len(b)
     return (gt - lt) / n
 
-delta = cliffs_delta(np.array(manual_durations), np.array(auto_durations))
-print("\nCliff's Delta:", delta)
 
-max_value = 18_000_000
-print(f'\n\nAnalysis for openings with length less than {max_value}:')
+def readable_time(time_ms):
+    seconds, ms = divmod(time_ms, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    return f"{days}d {hours}h {minutes}m {seconds}s"
+
+
+def print_analysis(auto_durations: list[int], manual_durations: list[int]):
+    print('-' * 50)
+    print(f'Auto openings: {len(auto_durations)}')
+    print(f'Manual openings: {len(manual_durations)}')
+
+    auto_median = np.median(auto_durations)
+    manual_median = np.median(manual_durations)
+
+    print(f'Median time for auto openings: {auto_median} ({readable_time(auto_median)})')
+    print(f'Median time for manual openings: {manual_median} ({readable_time(manual_median)})\n')
+
+    auto_average = np.average(auto_durations)
+    manual_average = np.average(manual_durations)
+
+    print(f'Average time for auto openings: {auto_average} ({readable_time(auto_average)})')
+    print(f'Average time for manual openings: {manual_average} ({readable_time(manual_average)})\n')
+
+    stat_small, p_value = mannwhitneyu(auto_durations, manual_durations, alternative='two-sided')
+    print(f"\nMann-Whitney p-value: {p_value}")
+    if p_value < 0.05:
+        print("The differences are statistically significant (p < 0.05)")
+    else:
+        print("There are no statistically significant differences")
+
+    p_value = permutation_test_median(np.array(auto_durations), np.array(manual_durations))
+    print("\nMedian permutation test p-value:", p_value)
+    if p_value < 0.05:
+        print("The differences are statistically significant (p < 0.05)")
+    else:
+        print("There are no statistically significant differences")
+
+    delta = cliffs_delta(np.array(manual_durations), np.array(auto_durations))
+    print("\nCliff's Delta:", delta)
+
+    print('-' * 50)
+
+
+print('Analysis for all data:')
+print_analysis(auto_durations, manual_durations)
+
+max_value = 18_000_000  # 5 hours
 
 auto_durations_small = [duration for duration in auto_durations if duration <= max_value]
-auto_durations_big = [duration for duration in auto_durations if duration > max_value]
-
 manual_durations_small = [duration for duration in manual_durations if duration <= max_value]
-manual_durations_big = [duration for duration in manual_durations if duration > max_value]
 
-print(f'{len(auto_durations_small)=}')
-print(f'{len(manual_durations_small)=}')
-
-stat_small, p_value_small = mannwhitneyu(auto_durations_small, manual_durations_small, alternative='two-sided')
-print(f"\nMann-Whitney p-value for small durations: {p_value_small}")
-
-p_value = permutation_test_median(np.array(auto_durations_small), np.array(manual_durations_small))
-print("\nPermutation test p-value for small durations:", p_value)
-if p_value < 0.05:
-    print("The differences are statistically significant (p < 0.05)")
-else:
-    print("There are no statistically significant differences")
-
+print(f'\n\nAnalysis for openings with length less than {max_value}:')
+print_analysis(auto_durations_small, manual_durations_small)
 
 # for all data:
-# plt.hist(manual_durations, bins=50, density=True, alpha=0.5, label='Manual')
-# plt.hist(auto_durations, bins=50, density=True, alpha=0.5, label='Auto')
+#plt.hist(manual_durations, bins=20, density=True, alpha=0.5, label='Manual')
+#plt.hist(auto_durations, bins=20, density=True, alpha=0.5, label='Auto')
 
 # only for short duration:
-plt.hist(manual_durations_small, bins=50, density=True, alpha=0.5, label='Manual')
-plt.hist(auto_durations_small, bins=50, density=True, alpha=0.5, label='Auto')
+plt.hist(manual_durations_small, bins=20, density=True, alpha=0.5, label='Manual')
+plt.hist(auto_durations_small, bins=20, density=True, alpha=0.5, label='Auto')
 
 plt.xlabel("Time")
 plt.ylabel("Amount")
 plt.title("Comparison of automatic and manual window opening times")
 plt.legend()
 plt.show()
+
+# boxplot for short duration
+# plt.boxplot([manual_durations_small, auto_durations_small], labels=["Manual", "Auto"])
+#
+# plt.title("Boxplot of window open durations")
+# plt.ylabel("Duration (ms)")
+# plt.show()
